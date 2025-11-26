17ec77a71f3f60b606a9428a353e7a3fefb67995
Athrva Bhawsar <athrvabh124@gmail.com>
2025-11-27 01:50:24 +0530

    final commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Athrva Bhawsar',
+    author_email= 'athrvabh124@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
